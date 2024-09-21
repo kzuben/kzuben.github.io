deef496ec1597e677ac2d912fd8824462be34c13
kimjiu <zubencode@gmail.com>
2024-09-21 11:51:24 +0900

    image

--- js/background.js
+++ js/background.js
@@ -1,17 +1,17 @@
 const images = [
-  "bg.png","bg2.png","bg3.jpg"
+  "bg.jpg","bg2.jpg","bg3.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
 
 console.log(chosenImage);
 const bgImage = document.createElement("img");
-bgImage.src = `images/${chosenImage}`;
+bgImage.src = `./images/${chosenImage}`;
 
 //document.body.appendChild(bgImage)
 
 const bg = document.querySelector(".page-wrapper");
-bg.style.background = "url(" + `images/${chosenImage}` + ") no-repeat center";
+bg.style.background = "url(" + `./images/${chosenImage}` + ") no-repeat center";
 bg.style.backgroundSize = "cover"
 console.log()
 
